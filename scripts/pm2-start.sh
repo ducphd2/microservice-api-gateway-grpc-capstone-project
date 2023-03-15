7b8d2d29a91c2116a723ad5ac12d9027b53a50e7
ducph <ducphd2@gmail.com>
2023-03-15 21:12:25 +0700

    Fix scripts

--- scripts/pm2-start.sh
+++ scripts/pm2-start.sh
@@ -2,4 +2,4 @@
 
 cd gateway && pm2 start dist/main.js --name gateway && cd -
 cd auth-service && pm2 start dist/main.js --name auth-service && cd -
-cd merchant-service && pm2 start dist/main.js --name merchant-service && cd -
+cd merchant-service-copy && pm2 start dist/main.js --name merchant-service && cd -
