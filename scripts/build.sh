eeb241855640772bb32c7b4a9c05a11d7f4f525a
Duc Pham Huynh <phamhuynhduc0212@gmail.com>
2023-03-15 21:12:56 +0700

    Fix scripts (#9)
    Co-authored-by: ducph <ducphd2@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 
 cd gateway && yarn build && cd -
 cd auth-service && yarn build && cd -
-cd merchant-service && yarn build && cd -
+cd merchant-service-copy && yarn build && cd -
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@
 
 cd gateway && yarn && cd -
 cd auth-service && yarn && cd -
-cd merchant-service && yarn && cd -
+cd merchant-service-copy && yarn && cd -
