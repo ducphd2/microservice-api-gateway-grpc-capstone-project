512422dbc4d06a4613c6a2ab13f749b5c06ffa8c
ducph <ducphd2@gmail.com>
2023-03-18 15:23:46 +0700

    Remove unused services

--- auth-service/src/interfaces/user/user.interface.ts
+++
@@ -1,5 +0,0 @@
-import { InputRegisterUserRequest } from './input-register.interface';
-
-export interface IUser extends Partial<InputRegisterUserRequest> {
-  id?: number;
-}
--- cv-service/src/types/inputCvIdRequest.ts
+++
@@ -1,3 +0,0 @@
-export interface InputCvIdRequest {
-  cvId: number;
-}
