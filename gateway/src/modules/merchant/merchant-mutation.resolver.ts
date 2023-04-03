107ad9eb30dbc903d81e21d6c66abc0eaa10ecd4
Duc Pham Huynh <phamhuynhduc0212@gmail.com>
2023-04-03 15:41:03 +0700

    Fix find merchant detail by id (#40)
    Co-authored-by: ducph <ducphd2@gmail.com>

--- gateway/src/modules/merchant/merchant-mutation.resolver.ts
+++ gateway/src/modules/merchant/merchant-mutation.resolver.ts
@@ -1,10 +1,10 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { MerchantService } from './merchant.service';
-import { QueryUtils } from '../../utils/query.utils';
-import { Merchant } from '../../types';
-import { GqlAuthGuard } from '../../guard';
 import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { RpcException } from '@nestjs/microservices';
+import { GqlAuthGuard } from '../../guard';
+import { Merchant } from '../../types';
+import { QueryUtils } from '../../utils/query.utils';
+import { MerchantService } from './merchant.service';
 
 @Resolver()
 export class MerchantMutationResolver {
@@ -14,7 +14,7 @@ export class MerchantMutationResolver {
   @UseGuards(GqlAuthGuard)
   async findMerchantById(@Args('id') id: number): Promise<Merchant> {
     try {
-      const result = await this.merchantService.findMerchantById({ id });
+      const result = await this.merchantService.findById({ id });
       return result;
     } catch (error) {
       throw new RpcException(error);
